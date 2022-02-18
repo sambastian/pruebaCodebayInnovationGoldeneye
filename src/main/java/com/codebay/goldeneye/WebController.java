5df32bb5c23b1bd9a711a5c74d42cc707a1591ff
Sam <samcruzdavila@gmail.com>
2022-02-18 17:24:49 +0000

    All form view and controller mapping

--- src/main/java/com/codebay/goldeneye/WebController.java
+++ src/main/java/com/codebay/goldeneye/WebController.java
@@ -1,14 +1,38 @@
 package com.codebay.goldeneye;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;  
 import org.springframework.ui.Model;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.codebay.goldeneye.bean.EmailBean;
+
 @Controller
 public class WebController {  
+	
     @GetMapping("/")
     public String index() {
+        return "redirect:/email_form";
+    }
+
+    @GetMapping("/email_form")
+    public String formGet(Model model) {
+    	model.addAttribute("officeLocationList", Arrays.asList(OfficeLocationEnum.values()));
         return "index";
     }
+
+    @PostMapping("/email_form")
+    public String formPost(EmailBean email, Model model) {
+    	model.addAttribute("officeLocationList", Arrays.asList(OfficeLocationEnum.values()));
+        model.addAttribute("email", reviewEmail(email));
+        return "index";
+    }
+    
+    public String reviewEmail(EmailBean email) {
+    	// TODO Implement the API when ready
+    	return email.toString();
+    }
+    
 }
